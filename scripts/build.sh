38e98c03b8063745fedbc029dc731635e3cb3c23
Andrew Pashinnik <tobirawork@gmail.com>
2019-01-31 22:07:54 +0200

    init

---
+++ scripts/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+mkdir build;
+cd build;
+cmake ..;
+make ./tetris_game;
+cd ..;
---
+++ scripts/run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cd ./build;
+make ./tetris_game;
+./tetris_game;
+cd ..;
